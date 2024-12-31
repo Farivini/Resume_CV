3dddab0638f2d38ea5abad7ccc243191a91abe46
Vinicius <64615200+Farivini@users.noreply.github.com>
2024-12-31 17:38:17 -0300

    Atualizando o CV com as últimas alterações

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 from pathlib import Path
-
 import streamlit as st
 from PIL import Image
-
+import streamlit.components.v1 as components
 
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
 resume_file = current_dir / "assets" / "CV.pdf"
 profile_pic = current_dir / "assets" / "profile-pic.png"
-whatsapp_number = "5541999132478"  # Substitua pelo seu número real
+whatsapp_number = "5541999132478"  # Replace with your real number
 whatsapp_image_url = "whatsapp.png"
 
 # --- GENERAL SETTINGS ---
@@ -17,81 +16,40 @@ PAGE_TITLE = "Digital CV | Vinicius Farineli Freire"
 PAGE_ICON = ":wave:"
 NAME = "Vinicius Farineli Freire"
 DESCRIPTION = """
-Consultor, formado em Big data e Engenharia da computação com Pós em Data scientist, apoiando a tomada de decisões baseadas em dados e automações de tarefas.
+Consultant, graduated in Big Data and Computer Engineering, with a Postgraduate degree in Data Science, supporting data-driven decision making and task automation.
 """
 EMAIL = "viniciusfarineli@email.com"
 
 SOCIAL_MEDIA = {
-    "Site": "",
+    "Website": "",
     "LinkedIn": "https://www.linkedin.com/in/vinicius-farineli/",
     "GitHub": "https://github.com/Farivini",
-    
 }
+
 PROJECTS = {
-    "Dashboard de Vendas de Carro": {
+    "Telegram bot + scrapping": {
         "Link": "https://www.linkedin.com/services/page/7142403115320a0782/",
-        "Descrição": "Desenvolvi um dashboard para acompanhar as vendas de carros, fornecendo métricas de retorno sobre o investimento (ROI) e monitoramento da evolução do faturamento ao longo do tempo. Este dashboard fornece insights valiosos para tomada de decisões estratégicas e identificação de oportunidades de crescimento.",
-        "Imagem": "vendas.png",
-        "Video": None
-    },
-    "Consolidação de KPIs em um App no Power BI": {
-        "Link": "Desenvolvimento interno da empresa",
-        "Descrição": "Responsável, junto com uma equipe dedicada, pela consolidação de todos os KPIs relevantes para monitoramento de rede em um aplicativo no Power BI. Este aplicativo integra mais de 50 relatórios individuais de power bi, usamos Mysql, Oracle, APIs e Power automate na construção dos paineis, todos elaborados por mim, abrangendo uma ampla gama de aspectos das operações de rede de telecomunicações. Essa centralização facilita a análise e tomada de decisões estratégicas, proporcionando uma visão abrangente do desempenho da rede.",
+        "Descrição": "Developed a Telegram bot capable of responding to user commands by performing web scraping to extract and return relevant information from web pages. The application can be utilized for various purposes, including price monitoring, news extraction, and specific data collection from websites.",
         "Imagem": None,
-        "Video": None
+        "Video": None,
     },
-    "Monitoramento KPI de Disponibilidade e Impacto de Paradas na Rede": {
-        "Link": "Desenvolvimento interno da empresa",
-        "Descrição": "Participei ativamente do desenvolvimento de um sistema que usa PHP Mysql com consultas a APIs e que usa sistema op linux, um sitema online que verifica o status dos elementos de rede, identificando se estão fora de serviço. Este sistema é essencial para garantir a disponibilidade contínua dos serviços de rede e a rápida resolução de problemas.",
+    "Pipeline GCP + Power bi":{
+        "Link": "https://github.com/Farivini/storage-dataflow-bigquery-powerbi",
+        "Descrição": "Developed a complete pipeline integrated Cloud storage , Data flow , Big query and Power bi",
         "Imagem": None,
-        "Video": None
-    },
+        "Video": None,
 
-     "Automação de inserção de nota fiscal no sistema": {
-        "Link": "https://www.linkedin.com/services/page/7142403115320a0782/",
-        "Descrição": "Implementei um bot que faz a inserção de mais de 600 notas fiscais no sistema interno da empresa de carros salvados",
-        "Imagem": None,
-        "Video": None
-    },
-
-    
-    "Bot de Consulta (Via Telegram)": {
-        "Link": "https://www.linkedin.com/services/page/7142403115320a0782/",
-        "Descrição": "Implementei um bot de consulta no aplicativo Telegram para fornecer suporte automatizado aos clientes. O bot é capaz de responder a consultas comuns dos clientes, fornecer informações sobre produtos e serviços, e encaminhar questões mais complexas para a equipe apropriada.",
-        "Imagem": "bot.png",
-        "Video": None
-    },
-    "Extração de Dados de PDF": {
-        "Link": "https://www.linkedin.com/services/page/7142403115320a0782/",
-        "Descrição": "Desenvolvi um sistema de extração de dados de arquivos PDF para automatizar o processo de coleta e análise de informações. Este sistema é capaz de extrair dados estruturados de documentos PDF, melhorando a eficiência e precisão na manipulação de grandes volumes de dados.",
-        "Imagem": None,
-        "Video": "https://www.youtube.com/watch?v=olP0nzFenkU"
-    },
-    "WebApp de Oficina Mecânica": {
-        "Link": "https://www.linkedin.com/services/page/7142403115320a0782/",
-        "Descrição": "Desenvolvi um WebApp para uma oficina mecânica, que permite o cadastro de clientes, busca desses clientes, e cadastro e busca de peças no estoque. Este WebApp otimiza os processos internos da oficina, proporcionando uma gestão eficiente de clientes e estoque de peças.",
-        "Imagem": None,
-        "Video":"https://www.youtube.com/watch?v=81edukb3RDc"
-    },
-    "WebApp Construído com Flutterflow": {
-        "Link": "https://www.linkedin.com/services/page/7142403115320a0782/",
-        "Descrição": "Utilizando a plataforma Flutterflow, desenvolvi um WebApp para oferecer uma experiência intuitiva aos usuários. Este WebApp oferece facilidade de compartilhar links importantes seu e do seu negocio.",
-        "Imagem": "webapp.png",
-        "Video": None
     }
 }
 
-
 st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 
-
-# --- LOAD CSS, PDF & PROFIL PIC ---
+# --- LOAD CSS, PDF & PROFILE PIC ---
 with open(css_file) as f:
     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 with open(resume_file, "rb") as pdf_file:
     PDFbyte = pdf_file.read()
 profile_pic = Image.open(profile_pic)
-
 
 # --- HERO SECTION ---
 col1, col2 = st.columns(2, gap="small")
@@ -101,89 +59,96 @@ with col1:
 with col2:
     st.title(NAME)
     st.write(DESCRIPTION)
-    img = st.image(whatsapp_image_url,width=20) 
-    wpp = st.markdown(f'[Me envie uma mensagem no WhatsApp](https://api.whatsapp.com/send?phone={whatsapp_number})', unsafe_allow_html=True)
+    img = st.image(whatsapp_image_url, width=20)
+    wpp = st.markdown(
+        f'[Send me a WhatsApp message](https://api.whatsapp.com/send?phone={whatsapp_number})',
+        unsafe_allow_html=True,
+    )
     st.write("📫", EMAIL)
-    # --- Botão de Download para o PDF ---
-   
-    download_button = st.download_button(
-    label="Download CV",
-    data=PDFbyte,
-    file_name="CV_Vinicius_Farineli_Freire.pdf",
-    mime="application/pdf"
-)
 
+    download_button = st.download_button(
+        label="Download CV",
+        data=PDFbyte,
+        file_name="CV_Vinicius_Farineli_Freire.pdf",
+        mime="application/pdf",
+    )
 
 # --- SOCIAL LINKS ---
-st.write('\n')
+st.write("\n")
 cols = st.columns(len(SOCIAL_MEDIA))
 for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
     cols[index].write(f"[{platform}]({link})")
 
-
 # --- EXPERIENCE & QUALIFICATIONS ---
-st.write('\n')
-st.subheader("Experiencia & Qualificação")
+st.write("\n")
+st.subheader("Experience & Qualifications")
 st.write(
     """
-- ✔️ 4 anos de experiência com extração e transformação de dados em bancos : SQL, Oracle RAC.
-
-- ✔️ 4 anos trabalhando com Python, Power bi, Azure, Kafka e R.
-
-- ✔️ 2 anos trabalhando com Big query e Data storage.
-
-- ✔️ Bom entendimento dos princípios estatísticos e suas respectivas aplicações
-
-- ✔️ Excelente trabalho em equipe e demonstração de forte senso de iniciativa em tarefas
-"""
+- ✔️ 4 years of experience in data extraction and transformation with SQL, Oracle RAC.
+- ✔️ 4 years working with Python, Power BI, Azure, Kafka, and R.
+- ✔️ 2 years of experience with Big Query and Data Storage.
+- ✔️ Strong understanding of statistical principles and their applications.
+- ✔️ Excellent team player with a strong sense of initiative.
+    """
 )
 
-
 # --- SKILLS ---
-st.write('\n')
+st.write("\n")
 st.subheader("Hard Skills")
 st.write(
     """
-- 👩‍💻 Programming: Python (Scikit-learn, Pandas),Pyspark, SQL, VBA, DAX e Django
-- 📊 Visualização e Ferramentas: PowerBi, MS Excel, Microstrategy, Plotly, Grafana, Apache spark, Apache Kafka e Power automate
-- 🗄️ Databases: Postgres, MongoDB, MySQL,Oracle, Big query.
+- 👩‍💻 Programming: Python (Scikit-learn, Pandas), Pyspark, SQL, VBA, DAX, and Streamlit
+- 📊 Data Visualization & Tools: Power BI, Looker, Excel, Microstrategy, Plotly, Grafana, Airflow, Power Automate
+- 🗄️ Databases: Postgres, MongoDB, MySQL, Oracle, Big Query, Neo4j.
 """
 )
 
-
 # --- WORK HISTORY ---
-st.write('\n')
-st.subheader("Trabalho")
+st.write("\n")
+st.subheader("Work Experience")
 st.write("---")
 
 # --- JOB 1
-st.write("🚧", "**Consultor na TIM **")
-st.write("01/2021 - Presente")
+st.write("🚧", "**Senior Consultant at TIM**")
+st.write("01/2021 - Present")
 st.write(
     """
-- ► Utilizei o PowerBI e SQL para redefinir e monitorar KPIs relacionados a rede de telecomunicações, fornecendo dados para paineis executivos para direcionamentos estrategicos.
-
-- ► Especializado em redefinição e criação de pipeline de dados com Kafka com criação de Dags.
-
-- ► Criação de automação de ingestão de dados atraves de API , envio de dados atraves do Azure e construção de Paineis executivos com Power bi.
-
-- ► Criação de modelos de ML com GCP - Big query, Data Storage e Vertex IA.
-
-- ► Participei de um projeto Yellow Belt cujo principal objetivo era melhorar a experiência do cliente através da otimização e priorização da manutenção de estações estratégicas para o negócio.
+- ► Utilized Power BI and SQL to redefine and monitor KPIs related to the telecommunications network, providing data for executive dashboards for strategic decision-making.
+- ► Specialized in redefining and creating data pipelines with Kafka, including creating Dags.
+- ► Automated data ingestion through APIs, sending data via Azure, and building executive dashboards with Power BI.
+- ► Developed ML models using GCP - Big Query, Data Storage, and Vertex AI.
+- ► Participated in a Yellow Belt project focused on improving the customer experience by optimizing and prioritizing maintenance of business-critical stations.
 """
 )
 
-
 # --- Projects & Accomplishments ---
-st.write('\n')
-st.subheader("Alguns Projetos e Desenvolvimentos")
+st.write("\n")
+st.subheader("Projects & Accomplishments")
 st.write("---")
 for project, details in PROJECTS.items():
     st.write(f"{project}:")
-    st.write(f"  - Descrição: {details['Descrição']}")
-    if details['Imagem']:
-        st.image(details['Imagem'], width=600)
-    if details['Video']:
-        st.video(details['Video'])
+    st.write(f"  - Description: {details['Descrição']}")
+    if details["Imagem"]:
+        st.image(details["Imagem"], width=600)
+    if details["Video"]:
+        st.video(details["Video"])
     st.write(f"  - Link: {details['Link']}")
-   
+
+# --- IFAMES ---
+st.write("\n")
+st.subheader("Interactive Dashboards")
+st.write("---")
+
+st.write("### Dashboard Pipeline GCP")
+components.iframe(
+    src="https://app.powerbi.com/view?r=eyJrIjoiNWU1MTM3OWEtOTY3Yi00NjM4LWE3MGItN2E1Nzg3Y2M3OWVkIiwidCI6ImRlNjFmOTAxLTI1ZTYtNDIxZi1hMjViLTAwZWM1ZjAzYmY3NSJ9",
+    width=600,
+    height=373,
+)
+
+st.write("### Dashboard Resume Big numbers")
+components.iframe(
+    src="https://app.powerbi.com/view?r=eyJrIjoiMzdiYzg4MzAtMmVhYy00MTI2LTgzZTYtNWM5MmQ2MjVlM2VlIiwidCI6ImRlNjFmOTAxLTI1ZTYtNDIxZi1hMjViLTAwZWM1ZjAzYmY3NSJ9",
+    width=600,
+    height=373,
+)
